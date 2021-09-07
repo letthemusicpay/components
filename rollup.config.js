48ee16d6eeebe6eb8498f3928d072194a1102eeb
ParamagicDev <konnor5456@gmail.com>
2021-09-06 20:47:13 -0400

    remove rollup, go back to modern, win

--- rollup.config.js
+++
@@ -1,9 +0,0 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import html from '@web/rollup-plugin-html';
-
-export default {
-  input: 'index.html',
-  output: { dir: 'demo' },
-  plugins: [nodeResolve(), html()],
-  external: ["https://cdn.skypack.dev/hls.js", "Hls"],
-};
