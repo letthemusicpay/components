e9a3090732e005f19c76334a394c9d18b5fadc36
ParamagicDev <konnor5456@gmail.com>
2021-10-12 00:04:34 -0400

    fix: add advanced error reporting.

--- src/helpers/hls.ts
+++ src/helpers/hls.ts
@@ -15,7 +15,7 @@ interface HlsInstance {
   destroy: () => void
 }
 
-export function attachHlsToTracks(tracks: HTMLMediaElement[]): void {
+export function attachHlsToTracks (tracks: HTMLMediaElement[]): void {
   tracks.forEach((track) => {
     if (track.dataset.hlsAttached === 'true') return
 
@@ -44,25 +44,25 @@ export function attachHlsToTracks(tracks: HTMLMediaElement[]): void {
 }
 
 // https://github.com/video-dev/hls.js/blob/master/docs/API.md#hlsrecovermediaerror
-function handleHlsErrors(hlsConstructor: HlsConstructor, hlsInstance: HlsInstance): void {
-  hlsInstance.on(hlsConstructor.Events.ERROR, function(_event: unknown, data: Record<string, unknown>) {
-    if (data.fatal) {
-      switch (data.type) {
-        case hlsConstructor.ErrorTypes.NETWORK_ERROR:
-          console.log('fatal network error encountered, attempting to recover...');
-          hlsInstance.startLoad();
-          break;
-        case hlsConstructor.ErrorTypes.MEDIA_ERROR:
-          console.log('fatal media error encountered, attempting to recover...');
-          hlsInstance.recoverMediaError();
-          break;
-        default:
-          // cannot recover
-          console.error("Fatal error occurred. Unable to recover.")
-          console.error(data)
-          hlsInstance.destroy();
-          break;
-      }
+function handleHlsErrors (hlsConstructor: HlsConstructor, hlsInstance: HlsInstance): void {
+  hlsInstance.on(hlsConstructor.Events.ERROR, function (_event: unknown, data: Record<string, unknown>) {
+    if (data.fatal === false) return
+
+    switch (data.type) {
+      case hlsConstructor.ErrorTypes.NETWORK_ERROR:
+        console.log('fatal network error encountered, attempting to recover...')
+        hlsInstance.startLoad()
+        break
+      case hlsConstructor.ErrorTypes.MEDIA_ERROR:
+        console.log('fatal media error encountered, attempting to recover...')
+        hlsInstance.recoverMediaError()
+        break
+      default:
+        // cannot recover
+        console.error('Fatal error occurred. Unable to recover.')
+        console.error(data)
+        hlsInstance.destroy()
+        break
     }
-  });
+  })
 }
