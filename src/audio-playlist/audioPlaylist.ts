672130d34bb15e6e8bbce4cc45e3d0908ed2dfe0
Konnor Rogers <konnor5456@gmail.com>
2021-10-28 01:00:10 -0400

    Update audioPlaylist.ts

--- src/audio-playlist/audioPlaylist.ts
+++ src/audio-playlist/audioPlaylist.ts
@@ -245,6 +245,7 @@ export class AudioPlaylist extends FASTElement {
 
     this.currentTrackElement.pause()
     this.dispatchEvent(this.trackPauseEvent)
+    window.setTimeout(() => this.tracks?.forEach((track) => track.pause())
     this.paused = true
     this.playing = false
   }
